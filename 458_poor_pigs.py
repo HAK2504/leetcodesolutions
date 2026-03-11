74aec72d9587261c10b1feda4437cdccbbd57200
DevHStack <32067966+DevHStack@users.noreply.github.com>
2026-03-11 12:07:24 +0100

    Add 458_poor_pigs.py - LeetCode Problem 458

---
+++ 458_poor_pigs.py
@@ -0,0 +1,11 @@
+import math
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        rounds = minutesToTest // minutesToDie
+        pigs = 0
+        
+        while (rounds + 1) ** pigs < buckets:
+            pigs += 1
+        
+        return pigs
